a71f2957da74db6ab11dd3a7f906dc3e73f21501
parth60k <parthmane140@gmail.com>
2026-02-10 16:06:29 +0530

    Add menu-driven flow to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,53 @@
 #include<iostream>
 #include "Library.h"
+  using namespace std;
+   
+void showMenu()
+{
+    cout<<"\n===== Library Management System =====\n";
+    cout<<"1. Add Book"<<endl;
+    cout<<"2. Add User"<<endl;
+    cout<<"3. Issue Book"<<endl;
+    cout<<"4. Return Book"<<endl;
+    cout<<"5. Display Books "<<endl;
+    cout<<"0. Exit "<<endl;
+    cout<<" Enter choice"<<endl;
+}
+
 int main()
 {
-    cout<<"Library Management System initialized"<<endl;
-    return 0;
+    Library library;
+    int choice;
+
+    do{
+        showMenu();
+        cin>>choice;
+
+        switch(choice)
+    {
+
+
+        case 1:
+            cout<<"Add Book selected"<<endl;
+            break;
+        case 2:
+            cout<<"Add User selected"<<endl;
+            break;
+        case 3:
+            cout<<"Issue Book selected"<<endl;
+            break;
+        case 4:
+            cout<<"Return Book selected"<<endl;
+            break;
+        case 5:
+            cout<<"Display Book selected"<<endl;
+            break;
+        case 0:
+            cout<<"Exiting Program..."<<endl;
+            break;    
+    }
+
+      }while(choice!=0);
+
+      return 0;
 }
